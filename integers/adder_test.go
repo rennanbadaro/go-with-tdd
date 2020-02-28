7050863406974befe19b5b8ed51c10509fa67d33
rennanbadaro <rennanrb@icloud.com>
2020-02-27 23:37:45 -0300

    Add fn example

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,15 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
 
 func TestAdder(t *testing.T) {
 	t.Run("Should properly add 2 + 2", func(t *testing.T) {
